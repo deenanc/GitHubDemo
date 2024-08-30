4b96501ee080fd84e7f78f15befbb26241ee3308
deenadhayalan <ddnc1@LAPTOP-LS9EEU1T>
2024-08-30 14:22:56 +0530

    second commit

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -3,7 +3,10 @@ package com.pages;
 public class LoginPage {
 	
 	public void loginPage(){
+		
 		System.out.println("This is login page");
+		
+		System.out.println("This is new comment");
 	}
 
 }
